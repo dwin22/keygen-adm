c81984f8471189e3a54c06fd0a80e39802c66cc2
dwin22 <49456607+dwin22@users.noreply.github.com>
2019-04-25 12:03:47 -0500

    Update instgerador.sh

--- instgerador.sh
+++ instgerador.sh
@@ -2,24 +2,12 @@
 IVAR="/etc/http-instas"
 SCPT_DIR="/etc/SCRIPT"
 rm $(pwd)/$0
-veryfy_fun () {
-[[ ! -d ${IVAR} ]] && touch ${IVAR}
-[[ ! -d ${SCPT_DIR} ]] && mkdir ${SCPT_DIR}
-unset ARQ
-case $1 in
-"gerar.sh")ARQ="/usr/bin/";;
-"http-server.py")ARQ="/bin/";;
-*)ARQ="${SCPT_DIR}/";;
-esac
-mv -f $HOME/$1 ${ARQ}/$1
-chmod +x ${ARQ}/$1
-}
 echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
 echo -e "\033[1;36m--------------------KEY GENERATOR By: ~Dw~--------------------------\033[0m"
 echo -e "\033[1;36m--------------------------------------------------------------------\033[0m"
 exit
 }
-[[ ! -e /usr/bin/trans ]] && wget -O /usr/bin/trans https://www.dropbox.com/s/l6iqf5xjtjmpdx5/trans?dl=0 &> /dev/null
+wget -O /usr/bin/trans https://www.dropbox.com/s/l6iqf5xjtjmpdx5/trans?dl=0 &> /dev/null
 mv -f /bin/http-server.py /bin/http-server.sh && chmod +x /bin/http-server.sh
 apt-get install bc -y &>/dev/null
 apt-get install screen -y &>/dev/null
